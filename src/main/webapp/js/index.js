aa385450dcb00d545173df38fdac3b71dd9af6ad
Administrator <gmh20052002@163.com>
2016-02-15 22:26:39 +0800

    用户登陆验证

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
 	$("#loginButton").click(function() {
-		cache: false, $.ajax({
-			type : "POST",
-			url : "/wlp/longin",
-			data : $('#loginForm').serialize(),
+		cache: false,
+		$.ajax({
+			type : "GET",
+			url : "/wlp/login",
+			data :{userName:$("#email").val(),password:$("#password").val()},
 			success : function(data) {
 				goTo('content.html');
-			}
+			},
+			 dataType: 'json'
 		});
 	});
 
