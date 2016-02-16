b7f5674e02f713dd36716adb95bd70cc2ccb71e1
gmh20052002 <gmh20052002@163.com>
2016-02-16 23:00:09 +0800

    提交激活码管理服务端接口代码

--- src/main/java/com/wlp/api/service/WlpActivecodeService.java
+++ src/main/java/com/wlp/api/service/WlpActivecodeService.java
@@ -2,29 +2,31 @@ package com.wlp.api.service;
 
 import java.util.List;
 
+import com.wlp.api.entity.WlpActivecode;
+
 public interface WlpActivecodeService {
 	/**
 	 * 获取用户可用激活码
 	 * @param email
 	 * @return
 	 */
-	public List<String> getCanUseActivecodes(String email);
+	public List<WlpActivecode> getCanUseActivecodes(String email);
 	/**
 	 * 获取用户已用激活码
 	 * @param email
 	 * @return
 	 */
-	public List<String> getUsedActivecodes(String email);
+	public List<WlpActivecode> getUsedActivecodes(String email);
 	/**
 	 * 获取用户分享的激活码
 	 * @param email
 	 * @return
 	 */
-	public List<String> getSharedActivecodes(String email);
+	public List<WlpActivecode> getSharedActivecodes(String email);
 	/**
 	 * 分享激活码
 	 * @param toEmail
 	 * @return
 	 */
-	public String shareActivecode(String toEmail);
+	public WlpActivecode shareActivecode(String fromEmail, String toEmail);
 }
