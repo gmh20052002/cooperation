bda531821a224a410e23bb7c79539b45abd060bf
hukangwei <kangwei.hu@chinacreator.com>
2016-03-20 23:46:13 +0800

    提交

--- src/main/java/com/wlp/web/UserController.java
+++ src/main/java/com/wlp/web/UserController.java
@@ -6,7 +6,7 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.Random; 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
